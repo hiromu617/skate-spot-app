8f5829eea1f7de51e0bdcb470d4b303ff9f51f67
hiro <hiromhiro617@gmail.com>
2021-06-01 22:46:14 +0900

    ヘッダーを作成

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from "@chakra-ui/react"
+import NavBar from '../components/NavBar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <NavBar/>
       <Component {...pageProps} />
     </ChakraProvider>
   )
