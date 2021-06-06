24ea4ac96a94ecce00238c0645db86b7fb42c4f8
hiro <hiromhiro617@gmail.com>
2021-06-06 12:32:33 +0900

    onAuthStateChangedの中の関数を修正

--- src/context/Auth.tsx
+++ src/context/Auth.tsx
@@ -1,43 +1,52 @@
-import firebase from 'firebase';
-import { FC, createContext, useEffect, useState } from 'react';
-import axios from '../../constants/axios'
-import {User} from '../../types/user'
+import firebase from "firebase";
+import { FC, createContext, useEffect, useState } from "react";
+import axios from "../../constants/axios";
+import { User } from "../../types/user";
 
-const AuthContext = createContext({} as{
-  currentUser: User | null | undefined
-  setCurrentUser: React.Dispatch<React.SetStateAction<User | null | undefined>>
-});
+const AuthContext = createContext(
+  {} as {
+    currentUser: User | null | undefined;
+    setCurrentUser: React.Dispatch<
+      React.SetStateAction<User | null | undefined>
+    >;
+  }
+);
 
 const AuthProvider: FC = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState<User | null | undefined>(
-    undefined
-  );
+  const [currentUser, setCurrentUser] =
+    useState<User | null | undefined>(undefined);
 
   useEffect(() => {
     // ログイン状態が変化するとfirebaseのauthメソッドを呼び出す
     firebase.auth().onAuthStateChanged(async (user) => {
-      await axios.post('/api/users/', {
-        user: {
-          uid: user?.uid,
-          name: user?.displayName
-        }
-      })
-      .then(res => {
-        console.log(res.data)
-        setCurrentUser(res.data);
-      })
-      .catch(e => console.log(e))
-      console.log(user?.uid)
-      console.log(user?.displayName)
-    })
+      console.log(user)
+      // userがnullでなければ(ログインしているならば)apiにpost
+      if (user) {
+        await axios
+          .post("/api/users/", {
+            user: {
+              uid: user?.uid,
+              name: user?.displayName,
+            },
+          })
+          .then((res) => {
+            setCurrentUser(res.data);
+          })
+          .catch((e) => console.log(e));
+      }else{
+        setCurrentUser(null);
+      }
+    });
   }, []);
 
   /* 下階層のコンポーネントをラップする */
   return (
-    <AuthContext.Provider value={{ currentUser: currentUser,  setCurrentUser:  setCurrentUser}}>
+    <AuthContext.Provider
+      value={{ currentUser: currentUser, setCurrentUser: setCurrentUser }}
+    >
       {children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
 
-export { AuthContext, AuthProvider }
+export { AuthContext, AuthProvider };
